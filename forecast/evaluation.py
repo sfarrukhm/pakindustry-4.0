feaf57564376ff2d90fcfa52bc2fbe3ad8115d25
sfarrukhm <en1.farrukh@gmail.com>
2025-09-25 10:01:04 +0500

    fix train.py and evaluation.py

--- forecast/evaluation.py
+++ forecast/evaluation.py
@@ -4,8 +4,6 @@ import json
 with open("results/val_metrics.json", "r") as f:
     results = json.load(f)
 
-print("===== Evaluation Results =====")
+print("===== Evaluation Results =====\n")
 print(f"sMAPE: {results['smape']:.2f}%")
 print(f"NRMSE: {results['nrmse']:.3f}")
-print(f"MAE:   {results['mae']:.2f}")
-print(f"RMSE:  {results['rmse']:.2f}")
